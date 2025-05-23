84eeaba29202ebc8132f95c30679324c4c31298e
nickmwangemi <nickmwangemi@gmail.com>
2025-05-23 11:54:05 +0300

    make closed_at field optional

--- integrations/github-cloud/github_integration/models/issue.py
+++ integrations/github-cloud/github_integration/models/issue.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import BaseModel
 
 
@@ -11,5 +13,5 @@ class Issue(BaseModel):
     body: str
     created_at: str
     updated_at: str
-    closed_at: str
+    closed_at: Optional[str]
     user: dict
