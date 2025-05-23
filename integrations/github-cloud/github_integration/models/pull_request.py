03a47e4065cbd4ea7511760e16b939bdb9150fc6
nickmwangemi <nickmwangemi@gmail.com>
2025-05-23 11:56:31 +0300

    make fields optional

--- integrations/github-cloud/github_integration/models/pull_request.py
+++ integrations/github-cloud/github_integration/models/pull_request.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import BaseModel
 
 
@@ -10,9 +12,9 @@ class PullRequest(BaseModel):
     html_url: str
     body: str
     created_at: str
-    updated_at: str
-    closed_at: str
-    merged_at: str
+    updated_at: Optional[str] = None
+    closed_at: Optional[str] = None
+    merged_at: Optional[str] = None
     user: dict
     base: dict
     head: dict
